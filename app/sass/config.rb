1ced3ec2d34460d7c4bc2ffc61ddbfc41767dc3e
bleonard <bodie.dev@gmail.com>
2014-04-02 00:07:37 -0500

    add node modules

---
+++ app/sass/config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+# Reference http://compass-style.org/help/tutorials/configuration-reference/
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "./www/css"
+sass_dir = "/app/sass"
+images_dir = "images"
+
+output_style = :compressed #:expanded or :nested or :compact or :compressed
+cache = false
+line_comments = false
+preferred_syntax = :sass
